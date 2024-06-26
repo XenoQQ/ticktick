fe787e467285bdabe46f7f4ec89214137983e3c5
Your Name <jeka199410@gmail.com>
2024-06-26 10:59:11 +0300

    new file:   src/assets/icon-sort.png 	modified:   src/components/todotoolbar.tsx

--- src/components/todotoolbar.tsx
+++ src/components/todotoolbar.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import IconSort from "../assets/icon-sort.png";
 
 const ToolbarButton = styled.div`
     position: absolute;
     right: 10px;
-    top: 65px;
+    top: 67px;
 
-    width: 50px;
-    height: 20px;
+    width: 25px;
+    height: 25px;
 
-    margin: 5px 0 10px 0;
+    background: no-repeat center/80% url(${IconSort});
 
     border: 3px solid #3b3b3b;
     border-radius: 5px;
 
     align-self: flex-end;
+
+    transition: 1s ease;
+
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.7;
+        transition: 0s;
+    }
 `;
 
 const ToolbarContainer = styled.div`
     position: absolute;
-    right: -150px;
-    top: 50px;
+    right: 44px;
+    top: 27px;
 
     display: flex;
     width: 140px;
-    height: 60px;
+    height: 65px;
     padding: 0 5px 0 5px;
 
     background-color: #202020;
@@ -35,16 +45,37 @@ const ToolbarContainer = styled.div`
 
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-evenly;
 `;
 
-const ToolbarOptionbutton = styled.div`
+const ToolbarOptionbutton = styled.button`
+    display: flex;
+
     width: 100%;
     height: 25px;
 
     border: 3px solid;
     border-radius: 5px;
     border-color: #3b3b3b;
+
+    background-color: #202020;
+
+    font-family: "Ubuntu", sans-serif;
+    color: #757575;
+    font-size: 15px;
+
+    align-items: center;
+    justify-content: center;
+
+    
+    transition: 1s ease;
+
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.7;
+        transition: 0s;
+    }
 `;
 
 const TodoToolbar: React.FC = () => {
@@ -58,8 +89,8 @@ const TodoToolbar: React.FC = () => {
             <ToolbarButton onClick={() => handleToolbarButtonClick()} />
             {toolbarVisible && (
                 <ToolbarContainer>
-                    <ToolbarOptionbutton />
-                    <ToolbarOptionbutton />
+                    <ToolbarOptionbutton>Группировать</ToolbarOptionbutton>
+                    <ToolbarOptionbutton>Сортировать</ToolbarOptionbutton>
                 </ToolbarContainer>
             )}
         </>
